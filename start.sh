23fbcc5a440d29603bbc7832e8cc9b5349c8d127
Furfa <aleksandr.myashin@gmail.com>
2021-08-31 23:58:01 +0700

    update start script

--- start.sh
+++ start.sh
@@ -4,9 +4,4 @@ pip install poetry
 
 poetry install
 
-cd backend
-poetry run ./manage.py runserver 0.0.0.0:8000 --insecure&
-
-cd ../frontend
-
-poetry run ./main.py && fg
+(trap 'kill 0' INT; (cd backend && poetry run ./manage.py runserver 0.0.0.0:8000 --insecure) & (cd frontend && poetry run ./main.py) )
